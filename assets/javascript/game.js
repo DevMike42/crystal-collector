21c966c7d72643deadbdd56d201ef294aa500a61
Mike Rivera <michaelrivera@michaels-mbp-2.attlocal.net>
2018-12-13 13:00:41 -0800

    HTML & Flex styling set up

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+// pseudo code
+
+// Game with 4 CRYSTALS
+// Each CRYSTAL should add a specific number to the TOTAL (when clicked)
+// Each CRYSTAL's number should be hidden from the user
+// When clicked, the TOTAL score should be updated
+
+// At the start of each round, computer generates a 
+// new RANDOM number between (19-120)
+
+// The player WINS if their total score matches the 
+// computer's number
+// The player LOSES if their total score goes over
+// the comuter's number
+
+// At the end of each ROUND, the game should reset
+// and the computer choses a NEW random number as
+// well as NEW hidden CRYSTAL values
+
+// The app should display the # of the player's WINS and 
+// LOSSES (DO NOT USE REFRESH AS A MEANS OF STARTING A 
+// NEW GAME)
+
+
+// GLOBAL VARRIABLES
+// ====================================================================
+
+
+
+// FUNCTIONS?
+// ====================================================================
+
+
+
+
+// If Statements
+// ====================================================================
+
