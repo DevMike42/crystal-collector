2ae2e279efadbffd680ece7d32408708bbab0af4
Mike Rivera <michaelrivera@michaels-mbp-2.attlocal.net>
2018-12-13 13:41:46 -0800

    psuedo coded logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,14 +25,78 @@
 // GLOBAL VARRIABLES
 // ====================================================================
 
+// Computer TOTAL & User's accruing TOTAL
+var computerNum = '';
+var userTotal = '';
+
+// Crystal values
+var crystal1 = '';
+var crystal2 = '';
+var crystal3 = '';
+var crystal4 = '';
+
+// Game counters
+var winCount = 0;
+var lossCount = 0;
 
 
-// FUNCTIONS?
+// FUNCTIONS
 // ====================================================================
 
 
+// startGame () 
+
+    // Reset userTotal back to 0
+
+    // Computer generates random number / Resets random number
+
+    // Generates new random values for all 4 CRYSTALS
+
+    // Make sure gameCounters don't reset to 0
+
+// --------------------------------------------------------------------
 
 
-// If Statements
+// collection ()
+
+    // Listen for button clicks on CRYSTALS
+
+    // Adds value of clicked CRYSTAL to userTotal
+
+// --------------------------------------------------------------------
+
+
+// totalChecker () * Possibly within collection ()
+
+    // Checks if userTotal = computerNum
+
+// --------------------------------------------------------------------
+
+
+// roundComplete () * Possibly within totalChecker ()
+
+    // If userTotal > computerNum then...
+
+        // update lossCount
+
+        // run startGame ()
+
+    // If usterTotal = computerNum then...
+
+        // update winCount
+
+        // run startGame ()
+
+    // else if
+
+        // run collection ()
+
+
+
+// MAIN PROCESS 
 // ====================================================================
 
+
+// startGame ()
+
+    // collection ()
